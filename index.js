43cd1a595873084530f209ffc067fc2a94ab3cf2
Seapassionmarine <anthonyeji25@gmail>
2024-08-14 01:27:48 -0700

    My code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const cors = require(`cors`)
-// const router = require('./router/')
+const router = require('./router/userRouter')
+const studentrouter = require ("./router/studentRouter")
 require('./config/dbconfig')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors({origin:"*"}))
-// app.use('/api/v1/user',router)
-app.use("/api/v1/", router)
-app.use("/api/v1/", studentrouter)
+app.use('/api/v1/user',router)
+app.use('/api/v1/user', studentrouter)
 
 
 const PORT = process.env.PORT || 4499
